7de2f7a73b07446b568e84d04192f9082a8884ec
Nathaniel Rudenberg <rnatou12@gmail.com>
2024-12-16 05:03:09 +0000

    initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "https://github.com/navarasu/onedark.nvim",
+    opts = {
+      style = "darker",
+    },
+  },
+  { "martinsione/darkplus.nvim" },
+  { "askfiy/visual_studio_code" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
