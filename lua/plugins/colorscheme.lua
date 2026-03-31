681c73d0e2ecfc19ce865bf278a62fe61b25af77
Nathaniel Rudenberg <rnatou12@gmail.com>
2026-03-31 14:49:54 -0400

    Change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,12 +10,20 @@ return {
   { "Mofiqul/vscode.nvim" },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { "rose-pine/neovim", name = "rose-pine" },
+  { "rebelot/kanagawa.nvim" },
+  {
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  { "projekt0n/github-nvim-theme", name = "github-theme" },
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "catppuccin-mocha",
       -- colorscheme = "vscode",
-      colorscheme = "rose-pine",
+      -- colorscheme = "rose-pine",
+      colorscheme = "github_dark_dimmed",
     },
   },
 }
