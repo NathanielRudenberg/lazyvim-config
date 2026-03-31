c397d660e5507b1cd2e1ba42707e9738f22c9241
Nathaniel Rudenberg <rnatou12@gmail.com>
2026-03-31 14:50:35 -0400

    Remove minuet as completion source

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -5,15 +5,16 @@ return {
       preset = "super-tab",
     },
     sources = {
-      default = { "lsp", "path", "buffer", "snippets", "minuet" },
+      -- default = { "lsp", "path", "buffer", "snippets", "minuet" },
+      default = { "lsp", "path", "buffer", "snippets" },
       providers = {
-        minuet = {
-          name = "minuet",
-          module = "minuet.blink",
-          async = true,
-          -- timeout_ms = 3000, -- matches minuet request_timeout (3s) * 1000
-          score_offset = 50, -- higher priority among suggestions
-        },
+        -- minuet = {
+        --   name = "minuet",
+        --   module = "minuet.blink",
+        --   async = true,
+        --   -- timeout_ms = 3000, -- matches minuet request_timeout (3s) * 1000
+        --   score_offset = 50, -- higher priority among suggestions
+        -- },
       },
     },
     -- Recommended to avoid unnecessary requests
