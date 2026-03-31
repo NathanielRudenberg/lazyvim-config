2c3db87748b8f9a4d9d45c8e7992fcef26f1d3de
Nathaniel Rudenberg <rnatou12@gmail.com>
2026-03-31 14:49:15 -0400

    Use Avante

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+      },
+    },
+  },
+  -- { "zbirenbaum/copilot-cmp", enabled = false },
+}
